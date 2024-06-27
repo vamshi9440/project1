49ebf51ba195b00039eeb0f6f0ad26f1c638ea33
vamshi krishna <mukkisavamshi@gmail.com>
2024-06-27 17:09:03 -0400

    hi

--- vamshi1/src/vamshi1/xyz/qwerty.java
+++ vamshi1/src/vamshi1/xyz/qwerty.java
@@ -3,7 +3,7 @@ package vamshi1.xyz;
 public class qwerty {
 	
 	public static void main(String args[]) {
-		System.out.print("Hello krishna Reddy");
+		System.out.print("Hello krishna Reddy123");
 	}
 
 }
